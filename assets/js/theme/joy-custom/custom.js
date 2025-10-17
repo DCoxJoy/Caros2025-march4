771680abdc181afe0c4b0fedddfc66e8245a9faa
David Cox <davidcox@David-Cox.local>
2025-10-16 21:50:00 -0700

    added cpa330s to restricted products list

--- assets/js/theme/joy-custom/custom.js
+++ assets/js/theme/joy-custom/custom.js
@@ -255,13 +255,14 @@ document.addEventListener("DOMContentLoaded", function () {
 //  RESTRICTED PRODUCTS CONFIG
 // ============================
 const RESTRICTED_PRODUCTS = {
-  ids: ['2160', '2163', '2166', '2055', '2089', '1893'], // Add/remove as needed
-  skus: ['HTA3021', 'HTA3023', 'HTA6024', 'HPA3024', 'CWM347MP', 'CWA657MP'], // Optional SKU match
+  ids: ['2160', '2163', '2166', '2055','2172','2089', '1893'], // Add/remove as needed
+  skus: ['HTA3021', 'HTA3023', 'HTA6024', 'HPA3024','CPA330S', 'CWM347MP', 'CWA657MP'], // Optional SKU match
   urlOverrides: {
     '2160': '/product/hta3021',
     'HTA3023': '/product/hta3023',
     'HTA6024': '/product/hta6024',
     'HPA3024': '/product/hpa3024',
+    'CPA330S': '/product/cpa330s',
     'CWM347MP': '/product/cwm347mp',
     'CWA657MP': '/product/cwa657mp'
   }
